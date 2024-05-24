be79947793c14ff7430e3a42a40223f3e7621737
Jhy9 <juusohyyrylainen@hotmail.com>
2024-05-24 14:25:15 +0300

    test test

---
+++ tests/contains-test.cpp
@@ -0,0 +1,44 @@
+#define BOOST_TEST_MODULE containstest
+#include "polygon.h"
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( vectorOutsideArea )
+{
+    Polygon testpoly;
+    testpoly.vertices = std::vector{
+        Vector2{10,10},
+        Vector2{20,10},
+        Vector2{20,20},
+        Vector2{10,20}
+    };
+    Vector2 testvector= {2,2};
+    BOOST_TEST(!testpoly.contains(testvector));
+}
+
+BOOST_AUTO_TEST_CASE( vectorInsideArea )
+{
+    Polygon testpoly;
+    testpoly.vertices = std::vector{
+        Vector2{10,10},
+        Vector2{20,10},
+        Vector2{20,20},
+        Vector2{10,20}
+    };
+    Vector2 testvector= {12,15};
+    BOOST_TEST(testpoly.contains(testvector));
+}
+
+BOOST_AUTO_TEST_CASE( vectorAlongsideBorder )
+{
+    Polygon testpoly;
+    testpoly.vertices = std::vector{
+        Vector2{10,10},
+        Vector2{20,10},
+        Vector2{20,20},
+        Vector2{10,20}
+    };
+    Vector2 testvector= {12,10};
+    BOOST_TEST(testpoly.contains(testvector));
+}
+
+
