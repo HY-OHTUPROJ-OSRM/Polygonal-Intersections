1a735324e5c97c1cfe6bbab1243d8007a5380945
pants64DS <pants64DS@users.noreply.github.com>
2024-05-24 14:34:25 +0300

    Fix multiple definition issue in unit tests

--- tests/contains-test.cpp
+++ tests/contains-test.cpp
@@ -1,6 +1,5 @@
-#define BOOST_TEST_MODULE containstest
 #include "polygon.h"
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( vectorOutsideArea )
 {
