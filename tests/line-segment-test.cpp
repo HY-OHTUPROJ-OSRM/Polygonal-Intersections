360326ad675a31f2d4b82c12c8ec0c96d7e89111
pants64DS <pants64DS@users.noreply.github.com>
2024-05-28 11:23:28 +0300

    Fix and rename find-first-interaction-test, convert spaces to tabs in unit tests

--- tests/line-segment-test.cpp
+++ tests/line-segment-test.cpp
@@ -2,30 +2,30 @@
 #include <boost/test/unit_test.hpp>
 
 struct inits {
-    LineSegment testsegment= {Vector2{1,2},Vector2{3,4}};
+	LineSegment testsegment= {Vector2{1,2},Vector2{3,4}};
 };
 
 BOOST_FIXTURE_TEST_SUITE( LineSegments, inits )
 
 BOOST_AUTO_TEST_CASE( segmentEval )
 {
-    Rational x = {2,3};
-    BOOST_TEST(testsegment.eval(x).x == 7);
-    BOOST_TEST(testsegment.eval(x).y == 10);
-    BOOST_TEST(testsegment.eval(x).z == 3);
+	Rational x = {2,3};
+	BOOST_TEST(testsegment.eval(x).x == 7);
+	BOOST_TEST(testsegment.eval(x).y == 10);
+	BOOST_TEST(testsegment.eval(x).z == 3);
 }
 
 BOOST_AUTO_TEST_CASE( segmentProjection )
 {
-    Vector2 vect1 = {5,9};
-    Vector2 vect2 = {2,4};
-    BOOST_TEST(testsegment.contains_projection(vect1) == false);
-    BOOST_TEST(testsegment.contains_projection(vect2) == true);
+	Vector2 vect1 = {5,9};
+	Vector2 vect2 = {2,4};
+	BOOST_TEST(testsegment.contains_projection(vect1) == false);
+	BOOST_TEST(testsegment.contains_projection(vect2) == true);
 }
 
 BOOST_AUTO_TEST_CASE( findIntersection )
 {
-    BOOST_TEST(1==1);
+	BOOST_TEST(1==1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,12 +6,12 @@
 static_assert(!std::same_as<Polygon, int>);
 
 BOOST_AUTO_TEST_CASE(test_case1) {
-    int x = 1;
-    int y = 2;
-    BOOST_TEST(x + y == 3);
+	int x = 1;
+	int y = 2;
+	BOOST_TEST(x + y == 3);
 }
 
 BOOST_AUTO_TEST_CASE(test_case2) {
-    std::string s = "Hello, world!";
-    BOOST_TEST(s.substr(0, 5) == "Hello");
+	std::string s = "Hello, world!";
+	BOOST_TEST(s.substr(0, 5) == "Hello");
 }
