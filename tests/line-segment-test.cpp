c8d7d41971d7c2a4252b8929158e15eab45365d9
Jhy9 <juusohyyrylainen@hotmail.com>
2024-05-28 23:18:50 +0300

    line segment, multipolygon tasks, tab fixed

--- tests/line-segment-test.cpp
+++ tests/line-segment-test.cpp
@@ -2,60 +2,60 @@
 #include <boost/test/unit_test.hpp>
 
 struct inits {
-		LineSegment testsegment= {Vector2{1,2},Vector2{3,4}};
+	LineSegment testsegment= {Vector2{1,2},Vector2{3,4}};
 };
 
 BOOST_FIXTURE_TEST_SUITE( LineSegments, inits )
 
 BOOST_AUTO_TEST_CASE( segmentEval )
 {
-		Rational x = {2,3};
-		BOOST_TEST(testsegment.eval(x).x == 7);
-		BOOST_TEST(testsegment.eval(x).y == 10);
-		BOOST_TEST(testsegment.eval(x).z == 3);
+	Rational x = {2,3};
+	BOOST_TEST(testsegment.eval(x).x == 7);
+	BOOST_TEST(testsegment.eval(x).y == 10);
+	BOOST_TEST(testsegment.eval(x).z == 3);
 }
 
 BOOST_AUTO_TEST_CASE( segmentProjection )
 {
-		Vector2 vect1 = {5,9};
-		Vector2 vect2 = {2,4};
-		BOOST_TEST(testsegment.contains_projection(vect1) == false);
-		BOOST_TEST(testsegment.contains_projection(vect2) == true);
+	Vector2 vect1 = {5,9};
+	Vector2 vect2 = {2,4};
+	BOOST_TEST(testsegment.contains_projection(vect1) == false);
+	BOOST_TEST(testsegment.contains_projection(vect2) == true);
 }
 
 BOOST_AUTO_TEST_CASE( findIntersectionParallel )
 {
-		LineSegment testsegment2= {Vector2{2,4},Vector2{4,6}};
-		if(find_intersection(testsegment,testsegment2).has_value())
-				{BOOST_FAIL("");}
+	LineSegment testsegment2= {Vector2{2,4},Vector2{4,6}};
+	if(find_intersection(testsegment,testsegment2).has_value())
+		{BOOST_FAIL("");}
 }
 
 BOOST_AUTO_TEST_CASE( findIntersectionParallelCollinearProjection )
 {
-		LineSegment testsegment2= {Vector2{0,1},Vector2{5,6}};
-		if(find_intersection(testsegment,testsegment2).has_value()){
-				BOOST_TEST(find_intersection(testsegment,testsegment2).value().nom == 0);
-				BOOST_TEST(find_intersection(testsegment,testsegment2).value().den == 1);
-		} else
-				{BOOST_FAIL("");}
+	LineSegment testsegment2= {Vector2{0,1},Vector2{5,6}};
+	if(find_intersection(testsegment,testsegment2).has_value()){
+		BOOST_TEST(find_intersection(testsegment,testsegment2).value().nom == 0);
+		BOOST_TEST(find_intersection(testsegment,testsegment2).value().den == 1);
+	} else
+		{BOOST_FAIL("");}
 }
 
 BOOST_AUTO_TEST_CASE( findIntersectionParallelColNoProjOverlap )
 {
-		LineSegment testsegment2= {Vector2{2,3},Vector2{9,10}};
-		if(find_intersection(testsegment,testsegment2).has_value()){
-				BOOST_TEST(find_intersection(testsegment,testsegment2).value().nom == 4);
-				BOOST_TEST(find_intersection(testsegment,testsegment2).value().den == 8);
-		} else
-				{BOOST_FAIL("");}
+	LineSegment testsegment2= {Vector2{2,3},Vector2{9,10}};
+	if(find_intersection(testsegment,testsegment2).has_value()){
+		BOOST_TEST(find_intersection(testsegment,testsegment2).value().nom == 4);
+		BOOST_TEST(find_intersection(testsegment,testsegment2).value().den == 8);
+	} else
+		{BOOST_FAIL("");}
 	
 }
 
 BOOST_AUTO_TEST_CASE( findIntersectionParallelColNoProjNoOverlap )
 {
-		LineSegment testsegment2= {Vector2{4,5},Vector2{9,10}};
-		if(find_intersection(testsegment,testsegment2).has_value())
-				{BOOST_FAIL("");}
+	LineSegment testsegment2= {Vector2{4,5},Vector2{9,10}};
+	if(find_intersection(testsegment,testsegment2).has_value())
+		{BOOST_FAIL("");}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/multipolygon-test.cpp
+++ tests/multipolygon-test.cpp
@@ -2,133 +2,133 @@
 #include <boost/test/unit_test.hpp>
 
 struct inits {
-		Polygon testpoly1;
-		void init_poly()
-		{
-				testpoly1.vertices = {
-						Vector2_32{10, 10},
-						Vector2_32{20, 10},
-						Vector2_32{20, 20},
-						Vector2_32{10, 20}
-				};
-		}
-		inits() : testpoly1() {init_poly();}
+	Polygon testpoly1;
+	void init_poly()
+	{
+		testpoly1.vertices = {
+			Vector2_32{10, 10},
+			Vector2_32{20, 10},
+			Vector2_32{20, 20},
+			Vector2_32{10, 20}
+		};
+	}
+	inits() : testpoly1() {init_poly();}
 };
 
 BOOST_FIXTURE_TEST_SUITE( Multipolygon, inits )
 
 BOOST_AUTO_TEST_CASE( multipolyContainsVector )
 {
-		Polygon testpoly2;
-		testpoly2.vertices = {
-				Vector2_32{30, 30},
-				Vector2_32{40, 30},
-				Vector2_32{40, 40},
-				Vector2_32{30, 40}
-		};
-		MultiPolygon multipoly;
-		multipoly.polygons = {testpoly1, testpoly2};
-		Vector2 testvector = {35,35};
-		BOOST_TEST(multipoly.contains(testvector));
+	Polygon testpoly2;
+	testpoly2.vertices = {
+		Vector2_32{30, 30},
+		Vector2_32{40, 30},
+		Vector2_32{40, 40},
+		Vector2_32{30, 40}
+	};
+	MultiPolygon multipoly;
+	multipoly.polygons = {testpoly1, testpoly2};
+	Vector2 testvector = {35,35};
+	BOOST_TEST(multipoly.contains(testvector));
 }
 
 BOOST_AUTO_TEST_CASE( multipolyDoesntContainVector )
 {
-		Polygon testpoly2;
-		testpoly2.vertices = {
-				Vector2_32{30, 30},
-				Vector2_32{40, 30},
-				Vector2_32{40, 40},
-				Vector2_32{30, 40}
-		};	
-		MultiPolygon multipoly;
-		multipoly.polygons = {testpoly1, testpoly2};
-		Vector2 testvector = {60,60};
-		BOOST_TEST(!(multipoly.contains(testvector)));
+	Polygon testpoly2;
+	testpoly2.vertices = {
+		Vector2_32{30, 30},
+		Vector2_32{40, 30},
+		Vector2_32{40, 40},
+		Vector2_32{30, 40}
+	};	
+	MultiPolygon multipoly;
+	multipoly.polygons = {testpoly1, testpoly2};
+	Vector2 testvector = {60,60};
+	BOOST_TEST(!(multipoly.contains(testvector)));
 }
 
 BOOST_AUTO_TEST_CASE( multipolyEdgeIntersects )
 {
-		Polygon testpoly2;
-		testpoly2.vertices = {
-				Vector2_32{30, 30},
-				Vector2_32{40, 30},
-				Vector2_32{40, 40},
-				Vector2_32{30, 40}
-		};
-		MultiPolygon multipoly;
-		multipoly.polygons = {testpoly1, testpoly2};
-		LineSegment testsegment = {Vector2{25,25},Vector2{35,40}};
-		BOOST_TEST((multipoly.edge_intersects(testsegment)));
+	Polygon testpoly2;
+	testpoly2.vertices = {
+		Vector2_32{30, 30},
+		Vector2_32{40, 30},
+		Vector2_32{40, 40},
+		Vector2_32{30, 40}
+	};
+	MultiPolygon multipoly;
+	multipoly.polygons = {testpoly1, testpoly2};
+	LineSegment testsegment = {Vector2{25,25},Vector2{35,40}};
+	BOOST_TEST((multipoly.edge_intersects(testsegment)));
 }
 
 BOOST_AUTO_TEST_CASE( multipolyEdgeDoesntIntersect )
 {
-		Polygon testpoly2;
-		testpoly2.vertices = {
-				Vector2_32{30, 30},
-				Vector2_32{40, 30},
-				Vector2_32{40, 40},
-				Vector2_32{30, 40}
-		};
-		MultiPolygon multipoly;
-		multipoly.polygons = {testpoly1, testpoly2};
-		LineSegment testsegment = {Vector2{25,5},Vector2{29,40}};
-		BOOST_TEST(!(multipoly.edge_intersects(testsegment)));
+	Polygon testpoly2;
+	testpoly2.vertices = {
+		Vector2_32{30, 30},
+		Vector2_32{40, 30},
+		Vector2_32{40, 40},
+		Vector2_32{30, 40}
+	};
+	MultiPolygon multipoly;
+	multipoly.polygons = {testpoly1, testpoly2};
+	LineSegment testsegment = {Vector2{25,5},Vector2{29,40}};
+	BOOST_TEST(!(multipoly.edge_intersects(testsegment)));
 }
 
 BOOST_AUTO_TEST_CASE( multipolyCountIntersectingSegmentsMany )
 {
-		Polygon testpoly2;
-		testpoly2.vertices = {
-				Vector2_32{30, 30},
-				Vector2_32{40, 30},
-				Vector2_32{40, 40},
-				Vector2_32{30, 40}
-		};
-		MultiPolygon multipoly;
-		multipoly.polygons = {testpoly1, testpoly2};
-		int a = 0;
-		PolygonalChain testchain;
-		testchain.vertices = {
-				Vector2_32{5, 15},
-				Vector2_32{15, 15},
-				Vector2_32{15, 35},
-				Vector2_32{55, 35},
-				Vector2_32{55, 55}
-		};
-		testchain.for_each_intersecting_segment(multipoly,[&](auto var){
-				(void) var;
-				a++;
-		});
-		BOOST_TEST(a==3);
+	Polygon testpoly2;
+	testpoly2.vertices = {
+		Vector2_32{30, 30},
+		Vector2_32{40, 30},
+		Vector2_32{40, 40},
+		Vector2_32{30, 40}
+	};
+	MultiPolygon multipoly;
+	multipoly.polygons = {testpoly1, testpoly2};
+	int a = 0;
+	PolygonalChain testchain;
+	testchain.vertices = {
+		Vector2_32{5, 15},
+		Vector2_32{15, 15},
+		Vector2_32{15, 35},
+		Vector2_32{55, 35},
+		Vector2_32{55, 55}
+	};
+	testchain.for_each_intersecting_segment(multipoly,[&](auto var){
+		(void) var;
+		a++;
+	});
+	BOOST_TEST(a==3);
 }
 
 BOOST_AUTO_TEST_CASE( multipolyCountIntersectingSegmentsNone )
 {
-		Polygon testpoly2;
-		testpoly2.vertices = {
-				Vector2_32{30, 30},
-				Vector2_32{40, 30},
-				Vector2_32{40, 40},
-				Vector2_32{30, 40}
-		};
-		MultiPolygon multipoly;
-		multipoly.polygons = {testpoly1, testpoly2};
-		PolygonalChain testchain;
-		testchain.vertices = {
-				Vector2_32{5, 5},
-				Vector2_32{5, 45},
-				Vector2_32{45, 45},
-				Vector2_32{55, 25},
-				Vector2_32{20, 25}
-		};
-		int a = 0;
-		testchain.for_each_intersecting_segment(multipoly,[&](auto var){
-				(void) var;
-				a++;
-		});
-		BOOST_TEST(a==0);
+	Polygon testpoly2;
+	testpoly2.vertices = {
+		Vector2_32{30, 30},
+		Vector2_32{40, 30},
+		Vector2_32{40, 40},
+		Vector2_32{30, 40}
+	};
+	MultiPolygon multipoly;
+	multipoly.polygons = {testpoly1, testpoly2};
+	PolygonalChain testchain;
+	testchain.vertices = {
+		Vector2_32{5, 5},
+		Vector2_32{5, 45},
+		Vector2_32{45, 45},
+		Vector2_32{55, 25},
+		Vector2_32{20, 25}
+	};
+	int a = 0;
+	testchain.for_each_intersecting_segment(multipoly,[&](auto var){
+		(void) var;
+		a++;
+	});
+	BOOST_TEST(a==0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
